d9c450c8a8e1f5a887aefb2f8313e7abb5029eea
ankith1998 <ajkarthik1315@gmail.com>
2020-08-19 23:47:43 +0530

    scroll view and parameters read added

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main()
   runApp(gkeapl());
 }
 class gkeapl extends StatelessWidget {
+  TextEditingController uname = TextEditingController();
+  TextEditingController password =TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,38 +15,46 @@ class gkeapl extends StatelessWidget {
         appBar: AppBar(
           title: Text("GKEAPL")
         ),
-          body: Container(
-            padding: EdgeInsets.all(15.0),
-            child: Column(
-              children: [
-                SizedBox(height: 200.0,),
-                Text("USERNAME"),
-                TextField(
-                  decoration: InputDecoration(
-                   prefixIcon: Icon(Icons.account_box),
-                    hintText: "Enter username",
-                      border: OutlineInputBorder()),
-                ),
-                SizedBox(height: 20.0,),
-                Text("PASSWORD"),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock),
-                    hintText: "Enter password"
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 200.0,),
+                  Text("USERNAME"),
+                  TextField(
+                    controller: uname,
+                    decoration: InputDecoration(
+                     prefixIcon: Icon(Icons.account_box),
+                      hintText: "Enter username",
+                        border: OutlineInputBorder()),
                   ),
-                ),
-                SizedBox(height: 20.0,),
-                RaisedButton(
-                  onPressed: (){
-                    print("Logged In Successfully");
-                  },
-                  color: Colors.lightBlue,
-                  child: Text("LOGIN"),
-                )
-              ],
-            ),
+                  SizedBox(height: 20.0,),
+                  Text("PASSWORD"),
+                  TextField(
+                    controller: password,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      hintText: "Enter password"
+                    ),
+                  ),
+                  SizedBox(height: 20.0,),
+                  RaisedButton(
+                    onPressed: (){
+                      var user = uname.text;
+                      var pass = password.text;
+                      print(user);
+                      print(pass);
+                      print("Logged In Successfully");
+                    },
+                    color: Colors.lightBlue,
+                    child: Text("LOGIN"),
+                  )
+                ],
+              ),
       ),
+          ),
       )
     );
   }
